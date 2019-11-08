f5bc55327f3e168509c623ad1b8dcbfb0003aa99
pangdoh <pang_doh@163.com>
2019-11-08 16:20:05 +0800

    1

---
+++ service/server.py
@@ -0,0 +1,53 @@
+import threading
+import socket
+
+from service.servers import httpserver
+
+
+# 创建服务
+class Server:
+
+    # 线程锁
+    lock = threading.Lock()
+    # 连接的客户端列表
+    clientList = []
+
+    # 启动
+    @classmethod
+    def run(cls, lst, server_type):
+        cls.server_type = server_type
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(lst)
+        s.listen(512)
+        while True:
+            ss, address = s.accept()
+            print('client %s is connection!' % (address[0]))
+            t = threading.Thread(target=cls.wait_connect, args=(ss, address,))
+            t.start()
+
+    # 等待连接
+    @classmethod
+    def wait_connect(cls, ss, address):
+        # 加入连接列表
+        cls.lock.acquire()
+        cls.clientList.append((ss, address))
+        cls.lock.release()
+        # 执行业务
+        cls.execute_work(ss)
+        # 关闭连接
+        ss.close()
+        # 移除连接列表
+        cls.lock.acquire()
+        cls.clientList.remove((ss, address))
+        cls.lock.release()
+
+    # 执行任务
+    @classmethod
+    def execute_work(cls, ss):
+        if cls.server_type == 'http':
+            print('- - - 启动HTTP服务 - - -')
+            httpserver.HttpServer(ss)
+        elif cls.server_type == 'proxy':
+            print('研发中')
+        elif cls.server_type == 'socks':
+            print('研发中')
